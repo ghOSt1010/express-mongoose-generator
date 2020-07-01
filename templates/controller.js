f3e3313bd7ea4ab6aefc09a26af6619173182b31
ghOSt1010 <43892935+ghOSt1010@users.noreply.github.com>
2020-07-01 11:46:32 +0200

    controller update

--- templates/controller.js
+++ templates/controller.js
@@ -73,16 +73,20 @@ class {modelNameController} {
     }
 
     static async remove(req, res, err){
+        if (!req.params.id) {
+         return await res.status(402).json({
+            message: 'MISSING_ARGUMENT',
+         });
+        }
+
         var id = req.params.id;
-        {modelName}.findByIdAndRemove(id, function (err, {name}) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when deleting the {name}.',
-                    error: err
-                });
-            }
-            return res.status(204).json();
-        });
+
+        try {
+            var data = await {modelNameService}.remove(id);
+            return await res.status(200).json(data);
+        } catch (err) {
+            return await res.status(500).json(err);
+        }
     }
 }
 
